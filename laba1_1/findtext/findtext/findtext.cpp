dda3aecec477e0c9c78e6a97da83c716225654b0
Lena2112 <Helen.MONSTRIK@gmail.com>
2016-09-21 08:34:16 +0300

    Revert "Исправлены замечания по коду"
    This reverts commit 1a48044a51efe28c263b79a0366ccea4a697f7d9.

--- laba1_1/findtext/findtext/findtext.cpp
+++ laba1_1/findtext/findtext/findtext.cpp
@@ -8,70 +8,64 @@ using namespace std;
 
 bool ValidateInput(int argc)
 {
-    if (argc != 3)
+	if (argc != 3)
     {
-        cout << "Invalid arguments count." << endl << 
-            "Usage: findtext.exe <file name> <text to search>" << endl;
-        return false;
+        cout << "Invalid arguments count.\n"<< 
+			"Usage: findtext.exe <file name> <text to search>\n";
+        return 0;
     }
-    return true;
+	return 1;
 }
 
-bool CheckingOfArguments(ifstream & input, const string & searchLine)
+bool ValidateTheVariables(ifstream &input, const string stringToFind)
 {
-    if (!input.is_open()) 
+	if (!input.is_open()) 
     {
-        cout << "Failed to open file for reading" << endl;
-        return false;
+        cout << "Failed to open file for reading\n";
+        return 0;
     }
 
-    if (searchLine.empty()) 
+	if (stringToFind == "") 
     {
-        cout << "It turned out that it is an empty string" << endl;
-        return false;
+        cout << "It turned out that it is an empty string\n";
+        return 0;
     }
-    
-    return true;
+	
+	return 1;
 }
 
-void SubstringSearch(ifstream & input, const string & searchLine)
+void SubstringSearch(ifstream &input, const string stringToFind)
 {
-    int lineNumber = 1;
-    bool isStringFound = false;
-    string readsLine;
+	int lineNumber = 1;
+    bool stringIsFound = false;
+    string readsAString;
 
-    while (getline(input, readsLine))
+	while (getline(input, readsAString))
     {
-        size_t position = readsLine.find(searchLine);
+        size_t position = readsAString.find(stringToFind);
         if (position != -1)
         {
-            cout << linenumber << endl;
-            isStringFound = true;
+            printf ("%d\n",lineNumber);
+            stringIsFound = true;
         }
         lineNumber++;
     }
 
-    if (!isStringFound)
-    {
+	if (!stringIsFound)
         cout << "Not found";
-    }
 }
 
 int main(int argc, char * argv[])
 {
-    setlocale(LC_ALL, "Rus");
+    setlocale (LC_ALL, "Rus");
 
     if(!ValidateInput(argc))
-    {
-        return 1;
-    }
+		return 1;
 
     ifstream input(argv[1]);
-    string searchLine = argv[2];
-    if (CheckingOfArguments(input, searchLine))
-    {
-        SubstringSearch(input, searchLine);
-    }
+    string stringToFind = argv[2];
+	if (ValidateTheVariables(input, stringToFind))	   
+        SubstringSearch(input, stringToFind);
 
     return 0;
 }
